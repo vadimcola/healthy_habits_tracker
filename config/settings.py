dde8daa74701f1800104895f9c9796c2745bfd2c
Vadim Andreev <vadimcola@mail.ru>
2023-10-09 22:26:23 +0700

    Сделана Регистрация и Авторизация пользователя

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'habits',
     'users',
@@ -128,3 +129,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
