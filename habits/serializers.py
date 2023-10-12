3597319426b9ff750c5d7807dfae1cd71ff8bc29
vadimcola <vadimcola@mail.ru>
2023-10-12 15:44:21 +0700

    Реализованы валидаторы

--- habits/serializers.py
+++ habits/serializers.py
@@ -7,9 +7,21 @@ from habits.validators import validate_time_to_complete, validate_period, valida
 class HabitSerializer(serializers.ModelSerializer):
     time_to_complete = serializers.IntegerField(validators=[validate_time_to_complete])
     period = serializers.IntegerField(validators=[validate_period])
-    related_habit = serializers.PrimaryKeyRelatedField(queryset=Habit.objects.all(), required=False,
-                                                       validators=[validate_related_habit])
+    related_habit = serializers.PrimaryKeyRelatedField(queryset=Habit.objects.all(),
+                                                       validators=[validate_related_habit],
+                                                       required=False)
 
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        related_habit = data.get('related_habit') # связанная привычка
+        reward = data.get('reward') # вознаграждение
+        is_pleasant = data.get('is_pleasant') # приятная привычка
+        if related_habit and reward:
+            raise serializers.ValidationError('Связанная привычка и вознаграждение не могут быть указаны одновременно')
+        elif is_pleasant and (reward is not None or related_habit is not None):
+            raise serializers.ValidationError(
+                "У приятной привычки не может быть вознаграждения или связанной привычки!")
+        return data
--- habits/validators.py
+++ habits/validators.py
@@ -4,14 +4,17 @@ from rest_framework import serializers
 
 def validate_time_to_complete(value):
     if value > 120:
-        raise ValidationError('Время выполнения должно быть не больше 120 секунд!')
+        raise serializers.ValidationError('Время выполнения должно быть не больше 120 секунд!')
 
 
 def validate_period(value):
     if value > 7:
-        raise ValidationError('Привычка не может быть выполнена реже, чем 1 раз в 7 дней!')
+        raise serializers.ValidationError('Привычка не может быть выполнена реже, чем 1 раз в 7 дней!')
 
 
 def validate_related_habit(value):
     if value and not value.is_pleasant:
         raise serializers.ValidationError('Связанная привычка должна быть приятной!')
+
+
+
