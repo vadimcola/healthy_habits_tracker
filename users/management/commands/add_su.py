b4057b509e23c9ebe18eb8989efbaf2b930aab44
Vadim Andreev <vadimcola@mail.ru>
2023-11-20 21:26:21 +0700

    Написан файл README.md

--- users/management/commands/add_su.py
+++ users/management/commands/add_su.py
@@ -6,7 +6,7 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='vadimcola@mail.ru',
+            email='test@test.ru',
             is_staff=True,
             is_superuser=True,
             is_active=True
